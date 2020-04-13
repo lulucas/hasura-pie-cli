891d4f74164edcae044de98a654270870e48aa43
ghost <ghost@void.com>
2020-04-14 00:15:46 +0800

    add config tpl

--- generator/app/tpl.go
+++ generator/app/tpl.go
@@ -160,4 +160,7 @@ const configYamlTpl = `version: 2
 admin_secret: 
 endpoint: http://localhost:8080
 metadata_directory: metadata
+actions:
+  kind: synchronous
+  handler_webhook_baseurl: {{ACTION_ENDPOINT}}
 `
